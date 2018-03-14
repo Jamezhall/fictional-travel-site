a3000d1ec166a296dbd2f08904cafb46d319b441
James <jamezhall@gmail.com>
2018-03-14 22:17:48 +1100

    Setup Postcss mixins for Gulp and responsive header image.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    cssImports = require('postcss-import');
+    cssImports = require('postcss-import'),
+    mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImports, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImports, mixins, cssvars, nested, autoprefixer]))
     .on('error', function (errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
